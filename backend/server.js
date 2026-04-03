a834a17fea049fd53c432956501ead20f93e4a69
Sougata-Nandy <sougatanandy12345@gmail.com>
2026-04-03 20:27:44 +0530

    feat: updated local branch with latest project structure from team

--- backend/server.js
+++ backend/server.js
@@ -24,3 +24,7 @@ sequelize.sync({ alter: true })
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Server on port ${PORT}`));
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
